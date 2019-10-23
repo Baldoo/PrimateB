79f0c2add705db8c3215d9f31767b0c7f3f5d8d7
baldo <baldo_campos@hotmail.com>
2019-10-22 17:20:33 -0700

    Baldo Commit 22/Oct

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GoToMainScene()
+    {
+        Debug.Log("Regresando al Menu");
+        SceneManager.LoadScene("PatronesMenu");
+    }
+
+    public void GotoGameScene()
+    {
+
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if(SceneManager.GetActiveScene().name == "Juego")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+            SceneManager.LoadScene("Juego");
+
+    }
+}
